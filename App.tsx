18db559b00131766bc40e90dc5c84769584ef068
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-12-15 22:30:52 +0900

    add

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import { Camera } from 'expo-camera';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
