59aea48fcdfa8e28e4929e38b438963c6893b248
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-12-15 22:24:10 +0900

    add

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { Camera } from 'expo-camera';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -5,6 +6,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Camera />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
